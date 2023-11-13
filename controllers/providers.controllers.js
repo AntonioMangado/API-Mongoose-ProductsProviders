cb7aaafbda5cdc471f4d88e5364eb5b66dbf3b83
AntonioMangado <antonio.mangado@gmail.com>
2023-11-13 16:55:31 +0400

    CREATE/ repo

---
+++ controllers/providers.controllers.js
@@ -0,0 +1,54 @@
+const Provider = require("../models/providers.models")
+
+    // Read
+    const getProvider = async (req, res) => {
+    try {
+        const id = req.params.id || "";
+        let providers = id ? await Provider.find({id}, "-_id -__v") : await Provider.find({}, "-_id -__v") ; //{}
+        res.status(200).json(providers); // Respuesta de la API para 1 producto
+    }
+    catch (error) {
+        console.log(`ERROR: ${error.stack}`);
+        res.status(400).json({msj:`ERROR: ${error.stack}`});
+    }
+    }
+
+    // CREATE
+    const createProvider = async (req, res) => {
+    console.log(req.body);
+
+    try{
+        const data = req.body;
+        let answer = await new Provider(data).save();
+        res.status(201).json(answer);
+
+    }catch (error) {
+        console.log(`ERROR: ${error.stack}`);
+        res.status(400).json({msj:`ERROR: ${error.stack}`});
+    }
+    }   
+
+    // Update
+    const updateProduct = (req, res) => {
+        res.status(200).send("Producto editado!");
+    }
+
+    // // Delete
+    // const deleteProvider = async (req, res) => {
+    //     try {
+    //         const name = req.params.name || "";
+    //         let providers = name ? await Provider.deleteOne({id}) : await Provider.find({}, "-_id -__v") ; //{}
+    //         res.status(200).json(providers); // Respuesta de la API para 1 producto
+    //     }
+    //     catch (error) {
+    //         console.log(`ERROR: ${error.stack}`);
+    //         res.status(400).json({msj:`ERROR: ${error.stack}`});
+    //     }
+    //     }
+
+    module.exports = {
+        getProvider,
+        updateProduct,
+        // deleteProvider,
+        createProvider
+    };
---
+++ server.js
@@ -0,0 +1,50 @@
+  // Variables globales
+  const express = require('express')
+  const app = express()
+  const port = 3000
+  const cowsay = require("cowsay")
+  
+  // Middlewares
+  const checkApiKey = require("./middleware/auth_api_key.js")
+  const morgan = require("./middleware/morgan.js")
+
+  // Logger
+  app.use(morgan(':method :host :status :param[id] - :response-time ms :body'));
+
+  // Pug
+  app.set('view engine', 'pug');
+  app.set('views','./views');
+
+  // Modulos de rutas
+  const productsApiRoutes = require("./routes/productsApi.routes.js")
+  const providersApiRoutes = require("./routes/providers.routes.js")
+
+
+  // Ruta de template
+  app.get('/', function(req, res){
+    res.send('API con Mongoose');
+  });
+
+  // Habilito recepción de JSON en servidor.
+  app.use(express.json()) 
+
+  // Rutas
+  // API
+  app.use("/api/products", productsApiRoutes)
+  app.use("/api/providers", providersApiRoutes)
+
+
+  // Última ruta por defecto. En caso de no encotrarse ninguna anterior, devolvemos un 404
+  app.get("*", (req,res) => { 
+      res.status(404).send("Gatito triste - 404 not found");
+  })
+
+  // Escuchador de event
+  app.listen(port, () => {
+    console.log(
+        cowsay.say({
+            text: `Nos vamos a por tortilla (si queda) Example app listening on port http://localhost:${port}`,
+            e: "oO",
+            T: "U "
+        }))
+  })
